c34b5c751a63601c6406899b1ee2ebbdc73c2bc0
Robby Bush <crobertbush@gmail.com>
2022-04-12 19:43:24 -0400

    homework

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
+//Variables
+
 var a = 10;
 var b = 20;
+
+
+
+//Addition
 
 function addSum(param1, param2) {
     console.log(param1 + param2);
 }
 addSum(a,b);
+
+
+
+//Subtraction
+
+function subDiff(param1, param2) {
+    console.log(param1-param2);
+}
+subDiff(a,b)
+
+
+
+//Multiplication
+
+function proSum(param1, param2) {
+    console.log(param1*param2);
+}
+proSum(a,b)
+
+
+
+//Division
+
+function quoDiff(param1, param2) {
+    console.log(param1/param2)
+}
+quoDiff(a,b)
